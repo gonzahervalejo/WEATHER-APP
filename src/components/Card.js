a6c14de781c78685a0eec8c9c114ad55d0232256
Your Name <you@example.com>
2023-04-18 21:02:17 -0300

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Spinner from "./Spinner";
+
+const Card = ({ loadingData, showData, weather, forecast }) => {
+  if (loadingData) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="mt-5">
+      {showData === true ? (
+        <div className="container">
+          <div className="card mb-3 mx-auto bg-black text-light">
+            <div className="row g-0">
+              <div className="col-md-4"></div>
+              <div className="col-md-8">
+                <div className="card-body text-start mt-2">
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h2 className="text-Light"> Sin datos</h2>
+      )}
+    </div>
+  );
+};
+export default Card;
---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/css/Spinner.css";
+const Spinner = () => {
+  return (
+    <div class="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Spinner;
